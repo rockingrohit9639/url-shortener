3d56f9a4bbb5554fbdb68c2abfad92b884aef161
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2020-12-11 20:57:04 +0530

    fixed bugs

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -2,15 +2,15 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.auth import views 
-from .views import home, redirection, shorten, dashboard, contactus, aboutus, shorten_for_logged_in_users
+from .views import home, redirection, shorten, dashboard, contactus, aboutus
 
 
 urlpatterns = [
     path('', home, name="Homepage"),
     path('short', shorten, name="Shorten URL"),
-    path('dashboard/shorten', shorten_for_logged_in_users, name="Shorten URL for Logged In Users"),
     path('<str:url>', redirection, name="Redirection To Original Page"),
     path('dashboard/', dashboard, name="DUS Dashboard"),
     path('aboutus/', aboutus, name="aboutus"),
     path('contactus/', contactus, name="contactus"),
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- mainApp/views.py
+++ mainApp/views.py
@@ -11,6 +11,7 @@ import string
 from django.utils import timezone
 from datetime import datetime, timedelta
 import os
+from django.conf import settings
 
 USED_FOR_MAPPING = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-"
 
@@ -31,6 +32,7 @@ def get_url():
 
 
 def home(request):
+    # print(request.META['HTTP_HOST'])
     # res = get_url()
     # for it in res:
     #     k = Keys(key=it)
@@ -39,18 +41,24 @@ def home(request):
 
 @login_required(login_url='/accounts/login/')
 def dashboard(request):
-    return render(request, "dashboard.html")
+    user = request.user
+    context = {}
+    all_urls = ShortenUrl.objects.filter(user=user)
+
+    context['all_urls'] = all_urls
+
+    return render(request, "dashboard.html", context)
 
 def redirection(request, url):
-    short_url = "https://duspy.herokuapp.com/" + request.path
+    short_url = str(request.META['HTTP_HOST']) + request.path
+    print(short_url)
     try:
         check = ShortenUrl.objects.get(short_url=short_url)
         if(check.expire_flag == False or datetime.now() < check.expiration_date):
             check.visits += 1
             check.save()
-            dashboard_logging(request)
 
-            return HttpResponseRedirect(check.original_url)
+            return redirect(check.original_url)
         else:
             check.expire_flag = True
             return render(request, 'expired.html')
@@ -58,8 +66,8 @@ def redirection(request, url):
         messages.error(request, "The URL you are trying to reach has been expired or it is not yet created.")
         return redirect("/")
 
-@login_required(login_url='/accounts/login/')
-def shorten_for_logged_in_users(request):
+
+def shorten(request):
     if request.method == "POST":
         if request.POST['original_url'] and request.POST['custom_url']:
             original = request.POST['original_url']
@@ -67,17 +75,15 @@ def shorten_for_logged_in_users(request):
             if org_url:
                 url = ShortenUrl.objects.get(original_url=original)
                 context = {
-                    "short_url":url,
+                    "short_url": url,
                 }
                 messages.error(request, "Short URL already exists. Please use the below URL.")
-                return render(request, 'dashboard.html', context)
+                return render(request, 'index.html', context)
             else:
                 original = request.POST['original_url']
-                expiry_days = request.POST['expire_days']
-                if not expiry_days:
-                    expiry_days = 7
+                expiry_days = 7
 
-                short = 'https://duspy.herokuapp.com/' + str(request.POST['custom_url'])
+                short = str(request.META['HTTP_HOST']) + "/" + str(request.POST['custom_url'])
                 check = ShortenUrl.objects.filter(short_url=short)
                 if not check:
                     newurl = ShortenUrl(
@@ -89,62 +95,34 @@ def shorten_for_logged_in_users(request):
                     )
                     newurl.save()
                     context = {
-                        "short_url":short,
+                        "short_url": short,
                     }
-                    return render(request, 'dashboard.html', context)
+                    return render(request, 'index.html', context)
                 else:
                     messages.error(request, "Custom URL already exists. Please use a different path.")
-                    return render(request, 'dashboard.html.html', { 'error' : 'Custom URL already exists'})
+                    return render(request, 'index.html', {'error': 'Custom URL already exists'})
         elif request.POST['original_url']:
             original = request.POST['original_url']
-            expiry_days = request.POST['expire_days']
-            if not expiry_days:
-                expiry_days = 7
+            expiry_days = 7
             org_url = ShortenUrl.objects.filter(original_url=original).exists()
             if org_url:
                 url = ShortenUrl.objects.get(original_url=original)
                 context = {
-                    "short_url":url,
-                }
-                return render(request, 'dashboard.html', context)
-            else:
-                short = 'https://duspy.herokuapp.com/'
-                key = Keys.objects.last()
-                short += key.key
-                key.delete()
-                newurl = ShortenUrl(original_url=original, short_url=short, user=request.user, creation_date=datetime.now(), expiration_date=(datetime.now() + timedelta(days=int(expiry_days))))
-                newurl.save()
-                context = {
-                    "short_url":short,
-                }
-                messages.success(request, "URL created successfully. Check below")
-                return render(request, 'dashboard.html', context)
-        else:
-            messages.error(request, "Empty Field")
-            return redirect('')
-    else:
-        return redirect('')
-
-def shorten(request):
-    if request.method == "POST":
-        if request.POST['original_url']:
-            original = request.POST['original_url']
-            org_url = ShortenUrl.objects.filter(original_url=original).exists()
-            if org_url:
-                url = ShortenUrl.objects.get(original_url=original)
-                context = {
-                    "short_url":url,
+                    "short_url": url,
                 }
                 return render(request, 'index.html', context)
             else:
-                short = 'https://duspy.herokuapp.com/'
+                
+                short = str(request.META['HTTP_HOST'])
                 key = Keys.objects.last()
-                short += key.key
+                short += "/" + key.key
                 key.delete()
-                newurl = ShortenUrl(original_url=original, short_url=short, user=request.user, creation_date=datetime.now(), expiration_date=datetime.now() + timedelta(days=7))
+                newurl = ShortenUrl(original_url=original, short_url=short, user=request.user,
+                                    creation_date=datetime.now(),
+                                    expiration_date=(datetime.now() + timedelta(days=int(expiry_days))))
                 newurl.save()
                 context = {
-                    "short_url":short,
+                    "short_url": short,
                 }
                 messages.success(request, "URL created successfully. Check below")
                 return render(request, 'index.html', context)
@@ -175,37 +153,3 @@ def contactus(request):
                         
 def aboutus(request):
     return render(request, "aboutus.html", {})
-
-
-
-
-###########################################   
-#       For Logging Dashboard Stats       #
-###########################################
-
-
-def dashboard_logging(request):
-    a = get_ip(request)
-    print (a)
-    print (os.environ[request.META.get('REMOTE_ADDR')])
-
-
-def get_ip(request):
-    
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
-    else:
-        ip = request.META.get('REMOTE_ADDR')
-    return ip
-
-
-    # g = GeoIP2()
-    # ip = request.META.get('REMOTE_ADDR', None)
-    # if ip:
-    #     city = g.city(ip)['city']
-    # else:
-    #     city = 'Rome' # default city
-
-    # return 
